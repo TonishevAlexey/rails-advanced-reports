ca0c8fdb432390cd90eef7f7a9e03a168c3ad2ed
aleksey <atonishev@phoenixit.ru>
2022-05-12 02:44:04 +0300

    part 8

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+  has_many_attached :files
+
   default_scope {order(best: "desc").order(created_at: "asc")}
   validates :body, presence: true
   def best_answer_false
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "questions#index"
-
+  resources :files, only: :destroy
   resources :questions, shallow: true do
     resources :answers, only: [:create, :update, :destroy] do
       patch :best, on: :member
