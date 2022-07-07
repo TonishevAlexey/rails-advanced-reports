8a767a030b9471bb678567ad296b843de52fa012
aleksey <atonishev@phoenixit.ru>
2022-07-07 18:06:35 +0300

    sidekiq pid

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -22,22 +22,26 @@ end
 preload_app true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
 
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
+      Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
     end
   end
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
-  child_pid = server.config[:pid].sub('.pid', ".#{worker.nr}.pid")
-  system("echo #{Process.pid} > #{child_pid}")
+  end
 end
